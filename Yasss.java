fc462cdc19a27b0bd21c31a239677d18deaba94c
ebonj <ebon.joiner@gmail.com>
2016-08-20 04:17:40 -0500

    Add files via upload

---
+++ Yasss.java
@@ -0,0 +1,16 @@
+import java.io.File;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+/
+public class Yasss
+{
+    public static void main(String[] args) throws FileNotFoundException
+    {
+        Scanner console = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(""); //insert file location
+        out.println(""); //insert message
+        out.close();
+    }
+        
+}
